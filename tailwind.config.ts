83b20c9b8765fd5a15fb3539048f971486609992
Ali Naeem <alibhaiplmnko60@gmail.com>
2024-12-09 22:04:54 +0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,17 +8,17 @@ export default {
   ],
   theme: {
     extend: {
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
+      // screens: {
+      //   sm: '640px',
+      //   md: '768px',
+      //   lg: '1024px',
+      //   xl: '1280px',
         colors: {
           background: "var(--background)",
           foreground: "var(--foreground)",
         },
     },
-  },
+  
 },
   plugins: [],
 } satisfies Config;
